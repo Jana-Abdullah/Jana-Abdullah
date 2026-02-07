332960b0536841eb74a410fb3b6f322a427d6dbb
Jana Abdullah <janaabdullah831@gmail.com>
2026-02-07 16:19:16 +0300

    Delete TechBD/Assign_Customers_To_Campaigns.sql
    Delete message

--- TechBD/Assign_Customers_To_Campaigns.sql
+++
@@ -1,9 +0,0 @@
---Solve Problem
-/*The query distributes customers 
-across marketing campaigns 
-using a modulo-based logic to 
-ensure balanced assignment and 
-avoid NULL campaign references.*/
-UPDATE Customers
-SET CampaignID =
-    (CustomerID % (SELECT COUNT(*) FROM MarketingCampaigns)) + 1;
